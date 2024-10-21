319d95834df3d42927e4d249afcc321a65e633c9
Jotaro15 <sterenn.alana@gmail.com>
2024-10-21 10:36:06 +0200

    Ajout addition
    Ajout dans le fichier Hello_world d'une fonction d'addition.

--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,8 @@
 # Est aussi le fichier main.py de l'exercice 2 de l'examen
 
-print ("Hello, World, it's us!")
+def addition(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    print("Hello, World, it's us!")
+    print(f"3 + 5 = {addition(3, 5)}")
