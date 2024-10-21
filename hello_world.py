c59f741798e9ce4bf52563b0e1a68b70f1aaf2ea
Jotaro15 <sterenn.alana@gmail.com>
2024-10-21 10:42:14 +0200

    Correction addition
    Ajout d'une vérification par type pour l'addition.

--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,10 @@
 # Est aussi le fichier main.py de l'exercice 2 de l'examen
 
-print ("Hello, World, it's us!")
+def addition(a, b):
+    if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
+        raise ValueError("Les deux arguments doivent être des nombres (int ou float)")
+    return a + b
+
+if __name__ == "__main__":
+    print("Hello, World, it's us!")
+    print(f"3 + 5 = {addition(3, 5)}")
